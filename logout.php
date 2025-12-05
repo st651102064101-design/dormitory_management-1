57f5b958e97771aa1c8bf573a2f2ee632d3fbecc
st651102064101 <st651102064101@pcru.ac.th>
2025-12-05 15:39:09 +0700

    Fix formatting by adding a space after the closing brace in logout.php

--- logout.php
+++ logout.php
@@ -8,7 +8,7 @@ if (ini_get('session.use_cookies')) {
     setcookie(session_name(), '', time() - 42000,
         $params['path'], $params['domain'], $params['secure'], $params['httponly']
     );
-} 
+}
 session_destroy();
 header('Location: Login.php');
 exit;
