6d5ff166649e0aae6da852e14b0688600817cf6c
kriangkrai <st651102064101@pcru.ac.th>
2025-12-25 11:02:44 +0700

    refactor: update require paths to use __DIR__ for better portability

--- Manage/save_public_theme.php
+++ Manage/save_public_theme.php
@@ -3,7 +3,7 @@
  * API สำหรับบันทึกธีมหน้าสาธารณะ
  */
 session_start();
-require_once '../ConnectDB.php';
+require_once __DIR__ . '/../ConnectDB.php';
 
 header('Content-Type: application/json');
 
