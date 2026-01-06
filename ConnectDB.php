4cc758b6117fbb4f3fd47925740f7e12bdc5124a
kriangkrai <st651102064101@pcru.ac.th>
2026-01-07 04:10:28 +0700

    Change database connection to XAMPP localhost

--- ConnectDB.php
+++ ConnectDB.php
@@ -4,11 +4,11 @@ date_default_timezone_set('Asia/Bangkok');
 
 if (!function_exists('connectDB')) {
     function connectDB(){
-        $host = 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com';
-        $port = '4000';
+        $host = 'localhost';
+        $port = '3306';
         $db   = 'dormitory_management_db';
-        $user = 'utqDEKTsPzQHUm4.root';
-        $pass = 'x5aHcQP0Y9m9gZMZ'; 
+        $user = 'root';
+        $pass = ''; 
 
         $dsn  = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 
@@ -16,9 +16,6 @@ if (!function_exists('connectDB')) {
             $options = [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                // ใช้ Path ตามที่หน้าจอ TiDB แนะนำ (รูปที่ 4)
-                PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/cert.pem',
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ];
 
             $pdo = new PDO($dsn, $user, $pass, $options);
