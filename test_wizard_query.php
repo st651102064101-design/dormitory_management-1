acd79bdae95a9984ecccf8a556dab4ef18193c5a
PHATPHIMONs <st651102064101@pcru.ac.th>
2026-05-05 05:31:21 +0700

    chore: Remove unused, test, and one-off scripts

--- test_wizard_query.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php';
-require_once __DIR__ . '/ConnectDB.php';
-require_once __DIR__ . '/includes/wizard_helper.php';
-
-$pdo = (new ConnectDB())->getConnection();
-$data = getWizardItems($pdo);
-print_r($data);
